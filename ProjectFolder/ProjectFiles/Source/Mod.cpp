85134bd628e404ddc89fa0ca42e4c355c337dda1
Albertbz <mail@albertbz.com>
2024-03-09 17:33:22 +0100

    Added setting of blocks using UE as well as undo

--- ProjectFolder/ProjectFiles/Source/Mod.cpp
+++ ProjectFolder/ProjectFiles/Source/Mod.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iostream>
 #include <filesystem>
+#include <stack>
 
 /************************************************************
 	Custom Variables for the mod
@@ -13,12 +14,102 @@ const int PlaceableCrystalBlockID = 1169799693;
 const int PlaceableGoldBlockID = 1169799694;
 const int PlaceableIronBlockID = 1169799695;
 
+const int Location1 = 1066913798;
+const int Location2 = 1823382047;
+const int BoxShape = 1919599248;
+const int SphereShape = 272127303;
+const int CylinderShape = 1174910537;
+const int PyramidShape = 210618550;
+const int RegisterFillType = 1004850721;
+const int Set = 666103118;
+const int Undo = 205046945;
+
+enum Shape { box, sphere, cylinder, pyramid };
+
+struct BlockInfoLocation {
+	BlockInfo type;
+	CoordinateInBlocks location;
+
+	BlockInfoLocation(BlockInfo type, CoordinateInBlocks location) {
+		this->type = type;
+		this->location = location;
+	}
+};
+
+CoordinateInBlocks location1 = CoordinateInBlocks(0, 0, 0);
+CoordinateInBlocks location2 = CoordinateInBlocks(0, 0, 0);
+Shape shape = box;
+BlockInfo fillType = BlockInfo(EBlockType::Air);
+std::stack<std::vector<BlockInfoLocation>> operations;
+
+int timesToIgnoreBlockPlacement = 0;
+bool registerFillType = false;
+
+/************************************************************
+	Custom Functions for the mod
+*************************************************************/
+
+void setBox() {
+	std::vector<BlockInfoLocation> operation;
+
+	int xMin = min(location1.X, location2.X);
+	int xMax = max(location1.X, location2.X);
+	int yMin = min(location1.Y, location2.Y);
+	int yMax = max(location1.Y, location2.Y);
+	int zMin = min(location1.Z, location2.Z);
+	int zMax = max(location1.Z, location2.Z);
+
+
+	for (int x = xMin; x <= xMax; x++) {
+		for (int y = yMin; y <= yMax; y++) {
+			for (int z = zMin; z <= zMax; z++) {
+				CoordinateInBlocks location = CoordinateInBlocks(x, y, z);
+				BlockInfo type = GetAndSetBlock(location, fillType);
+				operation.push_back(BlockInfoLocation(type, location));
+			}
+		}
+	}
+
+	operations.push(operation);
+}
+
+void setShape() {
+	switch (shape)
+	{
+	case box:
+		setBox();
+		break;
+	case sphere:
+		break;
+	case cylinder:
+		break;
+	case pyramid:
+		break;
+	default:
+		break;
+	}
+}
+
+void undoOperation() {
+	std::vector<BlockInfoLocation> operation = operations.top();
+
+	for (auto& block : operation) {
+		SetBlock(block.location, block.type);
+	}
+
+	operations.pop();
+}
+
 /************************************************************
 	Config Variables (Set these to whatever you need. They are automatically read by the game.)
 *************************************************************/
 
 UniqueID ThisModUniqueIDs[] = { PlaceableCoalBlockID, PlaceableCopperBlockID, PlaceableCrystalBlockID,
-								PlaceableGoldBlockID, PlaceableIronBlockID }; // All the UniqueIDs this mod manages. Functions like Event_BlockPlaced are only called for blocks of IDs mentioned here. 
+								PlaceableGoldBlockID, PlaceableIronBlockID,
+								Location1, Location2,
+								BoxShape, SphereShape, CylinderShape, PyramidShape,
+								RegisterFillType,
+								Set, Undo}; // All the UniqueIDs this mod manages. Functions like Event_BlockPlaced are only called for blocks of IDs mentioned here. 
 
 float TickRate = 0;							 // Set how many times per second Event_Tick() is called. 0 means the Event_Tick() function is never called.
 
@@ -44,6 +135,39 @@ void Event_BlockPlaced(CoordinateInBlocks At, UniqueID CustomBlockID, bool Moved
 		break;
 	case PlaceableIronBlockID:
 		SetBlock(At, EBlockType::Ore_Iron);
+		break;
+	case Location1:
+		Log(L"Storing location1: " + At.ToString());
+		location1 = At;
+		break;
+	case Location2:
+		Log(L"Storing location2: " + At.ToString());
+		location2 = At;
+		break;
+	case BoxShape:
+		Log(L"Setting shape to box");
+		shape = box;
+		break;
+	case SphereShape:
+		shape = sphere;
+		break;
+	case CylinderShape:
+		shape = cylinder;
+		break;
+	case PyramidShape:
+		shape = pyramid;
+		break;
+	case RegisterFillType:
+		Log(L"Getting ready to register fill type");
+		timesToIgnoreBlockPlacement = 2;
+		registerFillType = true;
+		break;
+	case Set:
+		Log(L"Setting shape");
+		setShape();
+		break;
+	case Undo:
+		undoOperation();
 		break;
 	}
 }
@@ -93,7 +217,16 @@ void Event_OnExit()
 // Run every time any block is placed by the player
 void Event_AnyBlockPlaced(CoordinateInBlocks At, BlockInfo Type, bool Moved)
 {
-
+	Log(L"Block placed: " + std::to_wstring((int)(Type.Type)) + L" " + std::to_wstring(Type.CustomBlockID) + L" at: " + At.ToString());
+	if (timesToIgnoreBlockPlacement != 0) {
+		Log(L"Ignoring block placement: " + std::to_wstring((int)(Type.Type)) + L" " + std::to_wstring(Type.CustomBlockID) + L" at: " + At.ToString());
+		timesToIgnoreBlockPlacement--;
+	}
+	else if (registerFillType) {
+		Log(L"Registering fill type: " + std::to_wstring((int)(Type.Type)) + L" " + std::to_wstring(Type.CustomBlockID) + L" at: " + At.ToString());
+		registerFillType = false;
+		fillType = Type;
+	}
 }
 
 // Run every time any block is destroyed by the player
